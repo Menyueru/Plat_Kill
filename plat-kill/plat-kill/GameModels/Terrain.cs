afffcc2efd5bbbdb7739eaa1e161edd3340a3bcf
Menyueru <Maddog28@gmail.com>
2013-06-24 00:15:30 -0400

    Fixing terrain

--- plat-kill/plat-kill/GameModels/Terrain.cs
+++ plat-kill/plat-kill/GameModels/Terrain.cs
@@ -16,19 +16,37 @@ namespace plat_kill.GameModels
         #region Properties
         private StaticMesh mesh;
         private Model model;
+        private Matrix world;
+        private Vector3 position;
         #endregion
         #region Methods
         #region Initialize
         public void LoadContent(ContentManager content, String path)
         {
+            position=new Vector3(0, -10, 0);
             model = content.Load<Model>(path);
             Vector3[] vertices;
             int[] indices;
             TriangleMesh.GetVerticesAndIndicesFromModel(model, out vertices, out indices);
-            mesh = new StaticMesh(vertices, indices, new AffineTransform(new Vector3(0, -10, 0)));
+            mesh = new StaticMesh(vertices, indices, new AffineTransform(position));
             mesh.Tag=model;
         }
         #endregion
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            world =  Matrix.CreateTranslation(position);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.Projection = projection;
+                    effect.View = view;
+                    effect.World = world;
+                }
+            }
+        }
         #endregion
     }
 }
